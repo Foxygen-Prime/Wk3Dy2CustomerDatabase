2035eb2cdc5b73962870547a1f91c8544d93b513
Courtney <courtney.trochim@gmail.com>
2017-08-01 14:03:50 -0500

    Saving Progress

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log((customers));
+const cust = customers.results;
+console.log(customers.results[0]);
+const cust = customers.results;
+console.log(cust[0].name.first);
+let firstName = cust[0].name.first;
+console.log(cust[0].name.last);
+let lastName = cust[0].name.last;
+let fullName = `${firstName} ${lastName}`;
+console.log('fullName', fullName);
+
+for (let i = 0; i < cust.length; i++) {
+  let firstName = cust[0].name.first;
+  let lastName = cust[0].name.last;
+  let fullName = `${firstName} ${lastName}`;
+  console.log('Customer Number', i+1, fullName);
+}
