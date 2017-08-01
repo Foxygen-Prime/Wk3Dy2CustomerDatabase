932d3d0e986059d5b53dc1aaf58f6df87b947de1
Courtney <courtney.trochim@gmail.com>
2017-08-01 15:52:15 -0500

    4pm Submission save

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 console.log((customers));
 const cust = customers.results;
 console.log(customers.results[0]);
-const cust = customers.results;
 console.log(cust[0].name.first);
 let firstName = cust[0].name.first;
 console.log(cust[0].name.last);
@@ -10,8 +9,8 @@ let fullName = `${firstName} ${lastName}`;
 console.log('fullName', fullName);
 
 for (let i = 0; i < cust.length; i++) {
-  let firstName = cust[0].name.first;
-  let lastName = cust[0].name.last;
+  let firstName = cust[i].name.first;
+  let lastName = cust[i].name.last;
   let fullName = `${firstName} ${lastName}`;
   console.log('Customer Number', i+1, fullName);
 }
